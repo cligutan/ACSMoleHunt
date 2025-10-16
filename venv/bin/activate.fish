4c8dff6252bdab1a68421c3ce129241b1d4f103a
cligutan <cfligutan@gmail.com>
2025-10-16 15:40:12 -0500

    idk

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/cheyanne/ACSMoleHunt/AcsMoleHunt/venv"
+set -gx VIRTUAL_ENV "/Users/cheyanne/ACSMoleHunt/AcsMoleHunt-1/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
